cf267acf94be914f1554ab8d93b1006a91c971cc
Dennis Saenger <mail@dennis.io>
2012-04-09 14:29:01 +0200

    updated Autoloader to include phannotation lib

--- Autoload.php
+++ Autoload.php
@@ -3,7 +3,8 @@ set_include_path(
 	get_include_path() . PATH_SEPARATOR .
 	dirname(__FILE__) . '/src' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/test' . PATH_SEPARATOR .
-	dirname(__FILE__) . '/examples'
+	dirname(__FILE__) . '/examples' . PATH_SEPARATOR .
+	dirname(__FILE__) . '/lib/phannotation/src/phannotation'
 );
 function autoload($clazz) {
 	$load = preg_replace('/\\\/', DIRECTORY_SEPARATOR, $clazz) . '.php';
