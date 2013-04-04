4985b0466095b398ed52df3beb68ba22b0cabec1
Dennis Saenger <mail@dennis.io>
2013-04-04 09:28:35 +0200

    adjusted paths in Autoloader

--- Autoload.php
+++ Autoload.php
@@ -4,10 +4,10 @@ set_include_path(
 	dirname(__FILE__) . '/src' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/test' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/examples' . PATH_SEPARATOR .
-	dirname(__FILE__) . '/lib/phannotation/src'
+	dirname(__FILE__) . '/lib/'
 );
 function autoload($clazz) {
-	$load = preg_replace('/\\\/', DIRECTORY_SEPARATOR, $clazz) . '.php';
+	$load = preg_replace('/\\\/', DIRECTORY_SEPARATOR, $clazz ) . '.php';
 	include_once($load);
 }
 spl_autoload_register('autoload');
